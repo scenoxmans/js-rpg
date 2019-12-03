cf52de748b6cf4e308c09d657f195d388c230190
scenoxmans <mansurnaurkhanov@mail.com>
2019-12-03 09:01:37 +0100

    pushing before the deadline

--- res/js/gamelogic.js
+++ res/js/gamelogic.js
@@ -1,1 +1,205 @@
+///Sections
+let characterSelector = document.getElementById('fight')
+let versus = document.getElementById('versus');
+    let left = document.getElementById('left');
+    let right = document.getElementById('right');
+    let lightning = document.getElementById('lightning');
+    let vs_logo = document.getElementById('vs');
+let round_1 = document.getElementById('round-1');
+let arena = document.getElementById('arena');
+let item_section = document.getElementById('choose-item');
+let hud = document.getElementById('hud');
+let hud_img = document.getElementById('hud-img');
 
+
+///Action Cam
+let actionCam = document.getElementById('actionCam')
+let enemy_actionCam = document.getElementById('enemy-actionCam')
+///Player chooses 
+let naruto = document.getElementById('naruto');
+let ichigo = document.getElementById('ichigo');
+///PLayer moves
+let hit = document.getElementById('hit');
+let shield = document.getElementById('shield');
+let special = document.getElementById('special');
+///Enemies
+let enemy_Health = document.getElementById('enemy-health');
+let madara = new Player("Madara", "fire", 5, 20, enemy_Health.value, 10);
+//Items
+let fire = document.getElementById('fire');
+let water = document.getElementById('water');
+let wind = document.getElementById('wind');
+///Naruto healthbar
+let naruto_Health = document.getElementById('naruto-health');
+///GameManager
+let naruto;
+let computer;
+//Play as Naruto
+function playAsNaruto(){
+    characterSelector.style.display = 'none';
+    item_section.style.display = 'block';
+    hud_img.src = 'res/img/narutoavatar.png';
+}
+function playAsIchigo(){
+    characterSelector.style.display = 'none';
+    item_section.style.display = 'block';
+    hud_img.src = 'res/img/ichigo-Avatar.png';
+}
+///Items
+function choosesFire(){ 
+    item_section.style.display = 'none';
+    naruto = new Player(name, "fire", 20, 30, naruto_Health.value, 40);
+    console.log(naruto)
+    round_1.style.display = 'initial';
+}
+function choosesWater(){
+    item_section.style.display = 'none';
+    naruto = new Player(name, "water", 20, 30, naruto_Health.value, 40);
+    round_1.style.display = 'initial';
+    console.log(naruto)
+}
+function choosesWind(){
+    item_section.style.display = 'none';
+    naruto = new Player(name, "wind", 20, 30, naruto_Health.value, 40);
+    round_1.style.display = 'initial';
+    console.log(naruto)
+}
+ ///naruto attack
+function narutoStrike(){
+    actionCam.style.background = 'url(res/img/naruto-attack.gif)';
+    actionCam.style.backgroundSize = 'cover';
+    hit.disabled = true;
+    special.disabled = true;
+    shield.disabled = true;
+    setTimeout(function(){
+        actionCam.style.background = 'none'
+        enemy_Health.value -= naruto.attack;
+        madaraAction();
+    },400)
+    if(naruto_Health.value <= 0){
+        alert("You've lost!");
+        location.reload();
+       } 
+ }
+ ///naruto blocks
+function narutoBlock(){
+    hit.disabled = true;
+    special.disabled = true;
+    shield.disabled = true;
+    madara.attack-= naruto.block;
+    actionCam.style.background = 'url(res/img/naruto-blocking.gif)';
+    actionCam.style.backgroundSize = 'cover';
+    setTimeout(function(){
+        actionCam.style.background = 'none';
+        madaraAction()
+        madara.attack = 15;
+
+    },1000)
+    if(naruto_Health.value <= 0){
+        alert("You've lost!");
+        location.reload();
+       } 
+ }
+ ///Naruto special
+ function specialing() {
+    hit.disabled = true;
+    special.disabled = true;
+    shield.disabled = true;
+    actionCam.style.background = 'url(res/img/naruto-special.gif)';
+    setTimeout(function(){
+        actionCam.style.background = 'none';
+        enemy_Health.value -= naruto.spAttack;
+        madaraAction()
+    },6700);
+    if(naruto_Health.value <= 0){
+        alert("You've lost!");
+        location.reload();
+       } 
+}
+///Madara actions
+function madaraAction(){
+    let action = [enemyAttacks(),enemySpecialing(),enemyBlocking()];
+    action = Math.floor(Math.random()*3);
+    
+    
+    if(enemy_Health.value <= 0){
+        alert("You've won!");
+        location.reload();
+       } 
+}
+function enemyAttacks(){
+    enemy_actionCam.style.background = 'url(res/img/madara-fireBalls.gif';
+    enemy_actionCam.style.backgroundSize = 'cover';
+    setTimeout(() => {
+        hit.disabled = false;
+        special.disabled = false;
+        shield.disabled = false;
+        enemy_actionCam.style.background = 'none';
+        naruto_Health.value -= madara.attack; 
+    }, 4000);
+    if(enemy_Health.value <= 0){
+        alert("You've won!");
+        location.reload();
+       } 
+}
+///Madara specialing
+function enemySpecialing(){
+    
+    naruto_Health.value -= madara.spAttack; 
+    
+
+    if(enemy_Health.value <= 0){
+        alert("You've won!");
+        location.reload();
+       } 
+}
+function enemyBlocking(){
+    hit.disabled = false;
+    special.disabled = false;
+    shield.disabled = false;
+    if (enemy_Health.value <=50){
+    enemy_actionCam.style.background = 'url(res/img/madara-blocking.gif';
+    enemy_actionCam.style.backgroundSize = 'cover';
+    setTimeout(() => {
+        enemy_actionCam.style.background = 'none';
+        naruto.attack-= 5;
+    }, 2500);
+
+ }
+}
+// function enemyHealing(){
+//     hit.disabled = false;
+//     special.disabled = false;
+//     shield.disabled = false;
+//     if (naruto_Health.value <= 20){
+//         enemy_actionCam.style.background = 'url(res/img/madara-healing.gif';
+//         enemy_actionCam.style.backgroundSize = 'cover';
+//     }
+//     setTimeout(() => {
+//         enemy_actionCam.style.background = 'none';
+//         enemy_Health.value += madara.attack + 5;
+//     }, 1500);
+
+// }
+
+
+
+///Object 
+function Player(name, item, attack, spAttack, health, block) {
+    this.name = name;
+    this.item = item;
+    this.attack = attack;
+    this.spAttack = spAttack;
+    this.health = health;
+    this.block = block;
+    if (this.item == 'fire'){
+        this.attack += 10;
+        this.spAttack += 10;
+    }
+    if (this.item == 'water'){
+        this.health += 30;
+    }
+    if (this.item == 'wind'){
+        this.block +=10;
+    }
+}
--- res/js/main.js
+++ res/js/main.js
@@ -1,135 +1,169 @@
 
-let ichigoImg = document.getElementById('ichigo');
-let narutoImg = document.getElementById('naruto');
-let changeBg = document.getElementById('fight');
-let actionCam = document.getElementById('actionCam')
+// let ichigoImg = document.getElementById('ichigo');
+// let narutoImg = document.getElementById('naruto');
+// let changeBg = document.getElementById('fight');
+// let actionCam = document.getElementById('actionCam')
 
-let characters = document.getElementById('characters');
-let narutoLogo = document.getElementById('narutoLogo');
-let makeChoice = document.getElementById('makeChoice');
-///Sections
-let loading = document.getElementById('loading');
-let versus = document.getElementById('versus');
-let round_1 = document.getElementById('round-1');
-let arena = document.getElementById('arena');
-///Player moves
-let hit = document.getElementById('hit');
-let heal = document.getElementById('heal');
-let shield = document.getElementById('shield');
-let special = document.getElementById('special');
+// let characters = document.getElementById('characters');
+// let narutoLogo = document.getElementById('narutoLogo');
+// let makeChoice = document.getElementById('makeChoice');
+// ///Sections
+// let loading = document.getElementById('loading');
+// let versus = document.getElementById('versus');
+//     let left = document.getElementById('left');
+//     let right = document.getElementById('right');
+//     let lightning = document.getElementById('lightning');
+//     let vs_logo = document.getElementById('vs');
+// let round_1 = document.getElementById('round-1');
+// let arena = document.getElementById('arena');
+// ///Player moves
+// let hit = document.getElementById('hit');
+// let heal = document.getElementById('heal');
+// let shield = document.getElementById('shield');
+// let special = document.getElementById('special');
 
-let attack = Math.floor(Math.random()*33); 
-let healingPoint = Math.floor(Math.random()*33); 
-let shieldedPoints = Math.floor(Math.random()*33); 
-///Enemy moves
-let enemyAttack = Math.floor(Math.random()*33); 
-let enemyHeal = Math.floor(Math.random()*33);
-let enemyShield = Math.floor(Math.random()*33);
-let enemyMoves = [enemyAttacking, enemyHealing, enemyShielding, enemySpecialing];
-///Health Bars
-let enemy_Health = document.getElementById('enemy-health');
-let naruto_Health = document.getElementById('naruto-health');
-///Events
-ichigoImg.addEventListener('click', clickIchigo);
-function clickIchigo(){
-    ichigoImg.src = 'res/img/ichigo-clicked.png';
-}
-narutoImg.addEventListener('click', clickNaruto);
-function clickNaruto(){
-    narutoImg.src = 'res/img/naruto-selected.gif';
-    narutoLogo.style.display = 'block';
-    setTimeout(function(){
-        cleanUp();
-    },1500)
-}
-///Clean up before Game Starts
-function cleanUp(){
- versus.style.display = 'none';
- makeChoice.style.display = 'none';
- characters.style.display = 'none';
- changeBg.style.background = 'url(res/img/arena.jpg)';
- changeBg.style.backgroundSize = 'cover';
- round_1.style.display = 'block';
-}
+// let attack = Math.floor(Math.random()*33); 
+// let healingPoint = Math.floor(Math.random()*33); 
+// let shieldedPoints = Math.floor(Math.random()*33); 
+// ///Enemy moves
+// let enemyAttack = Math.floor(Math.random()*33); 
+// let enemyHeal = Math.floor(Math.random()*33);
+// let enemyShield = Math.floor(Math.random()*33);
+// let enemyMoves = [enemyAttacking, enemyHealing, enemyShielding, enemySpecialing];
+// ///Health Bars
+// let enemy_Health = document.getElementById('enemy-health');
+// let naruto_Health = document.getElementById('naruto-health');
+// ///Events
+// ichigoImg.addEventListener('click', clickIchigo);
+// function clickIchigo(){
+//     ichigoImg.src = 'res/img/ichigo-clicked.png';
+// }
+// narutoImg.addEventListener('click', clickNaruto);
+// function clickNaruto(){
+//     narutoImg.src = 'res/img/naruto-selected.gif';
+//     narutoLogo.style.display = 'block';
+//     setTimeout(function(){
+//         versusScreen();
+//     },1500)
+// }
+// ///Clean up before Game Starts
+// function cleanUp(){
+//  versus.style.display = 'none';
+//  makeChoice.style.display = 'none';
+//  characters.style.display = 'none';
+//  changeBg.style.background = 'url(res/img/arena.jpg)';
+//  changeBg.style.backgroundSize = 'cover';
+//  round_1.style.display = 'block';
+// }
 
-///Naruto Attacks
+// ///Naruto Attacks
 
-hit.addEventListener('click', narutoStrike);
+// hit.addEventListener('click', narutoStrike);
 
-function narutoStrike(){
-   actionCam.style.background = 'url(res/img/naruto-attack.gif)';
-   actionCam.style.backgroundSize = 'cover';
-   hit.disabled = true;
-   setTimeout(function(){
-       actionCam.style.background = 'none'
-       enemy_Health.value -= attack;
-   },4000)
-   if(enemy_Health.value < 1){
-    alert("You've won!");
-    location.reload();
-   } 
-   enemyAction();
-}
-///Naruto Heals
-heal.addEventListener('click', healing)
+// function narutoStrike(){
+//    actionCam.style.background = 'url(res/img/naruto-attack.gif)';
+//    actionCam.style.backgroundSize = 'cover';
+//    setTimeout(function(){
+//        actionCam.style.background = 'none'
+//        enemy_Health.value -= attack;
+//    },4000)
+//    if(enemy_Health.value < 1){
+//     alert("You've won!");
+//     location.reload();
+//    } 
+//    enemyAction();
+// }
+// ///Naruto Heals
+// heal.addEventListener('click', healing)
 
-function healing() {
-    naruto_Health.value += healingPoint;
+// function healing() {
 
-    console.log(naruto_Health.value)
-}
-///Naruto Shields
-shield.addEventListener('click', shielding);
+//     actionCam.style.background = 'url(res/img/naruto-healing.gif)';
+//     actionCam.style.backgroundSize = 'cover';
+//     setTimeout(function(){
+//         actionCam.style.background = 'none'
+//         naruto_Health.value += healingPoint;
+//     },3000)
 
-function shielding(){
-    enemyAttack -= shieldedPoints;
-}
-///Naruto Speacial
-special.addEventListener('click', specialing);
+//     console.log(naruto_Health.value)
+// }
+// ///Naruto Shields
+// shield.addEventListener('click', shielding);
 
-function specialing(){
-    actionCam.style.background = 'url(res/img/naruto-special.gif)';
-    special.disabled = true;
-    setTimeout(function(){
-        actionCam.style.background = 'none';
-        enemy_Health.value -= attack * 2;
-    },6700);
-    if(enemy_Health.value < 1){
-        alert("You've won!");
-        location.reload();
-       } 
-       enemyAction();
-}
-///Enemy attacks
+// function shielding(){
+//     enemyAttack -= shieldedPoints;
+//     actionCam.style.background = 'url(res/img/naruto-blocking.gif)';
+//     actionCam.style.backgroundSize = 'cover';
+//     setTimeout(function(){
+//         actionCam.style.background = 'none'
+//     },1000)
 
-function enemyAction() {
-    enemyMoves = Math.floor(Math.random().lenght);
-}
-function enemyAttacking(){
-    naruto_Health.value -= enemyAttack;
-}
-function enemyHealing(){
-    enemy_Health.value += enemyHeal;
-}
-function enemyShielding(){
-    enemyShield = attack;
-    enemyShield -= attack;
-}
-function enemySpecialing(){
-    let enemySpecial = enemyAttack * 2;
-    naruto_Health.value -= enemySpecial;
-}
+// }
+// ///Naruto Speacial
+// special.addEventListener('click', specialing);
 
-console.log(attack);
+// function specialing(){
+//     actionCam.style.background = 'url(res/img/naruto-special.gif)';
+//     setTimeout(function(){
+//         actionCam.style.background = 'none';
+//         enemy_Health.value -= attack * 2;
+//     },6700);
+//     if(enemy_Health.value < 1){
+//         alert("You've won!");
+//         location.reload();
+//        } 
+//        enemyAction();
+// }
+// ///Enemy attacks
+
+// function enemyAction() {
+//     enemyMoves = Math.floor(Math.random().lenght);
+// }
+// function enemyAttacking(){
+//     naruto_Health.value -= enemyAttack;
+// }
+// function enemyHealing(){
+//     enemy_Health.value += enemyHeal;
+// }
+// function enemyShielding(){
+//     enemyShield = attack;
+//     enemyShield -= attack;
+// }
+// function enemySpecialing(){
+//     let enemySpecial = enemyAttack * 2;
+//     naruto_Health.value -= enemySpecial;
+// }
+
+// function versusScreen(){
+//     left.style.transform = 'translateX(0%)';
+//     right.style.transform = 'translateX(0%)';
+//     vs.style.transform = 'translateY(0%)';
+//     lightning.style.transform = 'translateY(0%)';
+//     versus.style.display = 'flex';
+//     makeChoice.style.display = 'none';
+//     characters.style.display = 'none';
+//     setTimeout(function(){
+//         cleanUp();
+
+//     },2000)
+// }
 
 
 
+// // let enemyHit = document.getElementById('enemy-hit');
+// // let enemyHeal = document.getElementById('enemy-heal');
+// // let enemyShield = document.getElementById('enemy-shield');
+// // let enemySpecial = document.getElementById('enemy-speacial');
 
 
+// function Person(name, item, attack, spAttack, health, block) {
+//     this.name = name;
+//     this.item = item;
+//     this.attack = attack;
+//     this.spAttack = spAttack;
+//     this.health = health;
+//     this.block = block;
+// }
 
 
-
-// let enemyHit = document.getElementById('enemy-hit');
-// let enemyHeal = document.getElementById('enemy-heal');
-// let enemyShield = document.getElementById('enemy-shield');
-// let enemySpecial = document.getElementById('enemy-speacial');
+// let naruto = new Person("Naruto", item, 5, 20, 200, 35, 200);
